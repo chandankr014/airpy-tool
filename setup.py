c31c736e072188b3c5385d39821f1b453ebd9b84
Chandan Kumar <chandankr014@gmail.com>
2025-05-14 02:55:47 +0530

    updated yml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="airpy-tool",
-    version="1.0.1",
+    version="1.0.0",
     packages=find_packages(include=['airpy', 'airpy.*']),
     include_package_data=True,
     package_data={
